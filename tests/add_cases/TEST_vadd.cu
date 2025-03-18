a668fe7a9179c851e86fdc649c323f65efaf9b97
zhangzhe <18890092219@163.com>
2025-03-18 19:57:24 +0800

    ✨ feat(Project): support cuda
    In CMakeLists.txt, set(CMAKE_CUDA_FLAGS "${CMAKE_CUDA_FLAGS} -G -g") to support debug CUDA kernel

---
+++ tests/add_cases/TEST_vadd.cu
@@ -0,0 +1,71 @@
+/**
+ * @file TEST_vadd.cu
+ * @author zhe.zhang
+ * @date 2025-03-18 16:20:18
+ * @brief
+ * @attention
+ */
+
+#include "vadd.h"
+
+#include <gtest/gtest.h>
+#include <iostream>
+#include "common.h"
+
+TEST(vadd, length_1024)
+{
+    const uint32_t N = 1024;
+    float *h_A, *h_B, *h_C;
+
+    CHECK(cudaMallocHost(&h_A, N * sizeof(float)));
+    CHECK(cudaMallocHost(&h_B, N * sizeof(float)));
+    CHECK(cudaMallocHost(&h_C, N * sizeof(float)));
+
+    for (uint32_t i = 0; i < N; i++)
+    {
+        h_A[i] = static_cast<float>(i);
+        h_B[i] = static_cast<float>(i * 2);
+    }
+
+    float *d_A, *d_B, *d_C;
+    CHECK(cudaMalloc((void **)&d_A, N * sizeof(float)));
+    CHECK(cudaMalloc((void **)&d_B, N * sizeof(float)));
+    CHECK(cudaMalloc((void **)&d_C, N * sizeof(float)));
+
+    CHECK(cudaMemcpy(d_A, h_A, N * sizeof(float), cudaMemcpyHostToDevice));
+    CHECK(cudaMemcpy(d_B, h_B, N * sizeof(float), cudaMemcpyHostToDevice));
+
+    int threadsPerBlock = 128;
+    int blocksPerGrid = (N + threadsPerBlock - 1) / threadsPerBlock;
+    vectorAdd<<<blocksPerGrid, threadsPerBlock>>>(d_A, d_B, d_C, N);
+
+    cudaDeviceSynchronize();
+
+    CHECK(cudaMemcpy(h_C, d_C, N * sizeof(float), cudaMemcpyDeviceToHost));
+
+    // 验证结果
+    bool correct = true;
+    for (uint32_t i = 0; i < N; i++)
+    {
+        if (h_C[i] != h_A[i] + h_B[i])
+        {
+            correct = false;
+            std::cout << "Error at index " << i << ": " << h_C[i] << " != " << h_A[i] + h_B[i] << std::endl;
+            break;
+        }
+    }
+
+    if (correct)
+    {
+        std::cout << "Vector addition successful!" << std::endl;
+    }
+
+    CHECK(cudaFree(d_A));
+    CHECK(cudaFree(d_B));
+    CHECK(cudaFree(d_C));
+    CHECK(cudaFreeHost(h_A));
+    CHECK(cudaFreeHost(h_B));
+    CHECK(cudaFreeHost(h_C));
+
+    ASSERT_TRUE(correct);
+}
